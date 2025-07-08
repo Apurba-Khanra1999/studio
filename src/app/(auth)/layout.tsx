5cda7fd68d2a92b78f778a1309801f03fd50d4d2
APURBA KHANRA <Apurbakhanra09@gmail.com>
2025-07-08 16:19:25 +0000

    use some good images from unsplash.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,9 +13,8 @@ export default function AuthLayout({
             </div>
             <div className="hidden bg-muted lg:block">
                 <Image
-                    src="https://placehold.co/1920x1080.png"
-                    data-ai-hint="productivity illustration"
-                    alt="Image"
+                    src="https://images.unsplash.com/photo-1497215728101-856f4ea42174?q=80&w=1920&auto=format&fit=crop"
+                    alt="A modern office space with large windows and plants"
                     width="1920"
                     height="1080"
                     className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"

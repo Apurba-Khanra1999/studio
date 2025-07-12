da743fd99ac0b2b822f6c36fde2c80523d5f4d64
Apurba Khanra <77966841+Apurba-Khanra1999@users.noreply.github.com>
2025-07-12 11:36:05 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dev.inktagon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   allowedDevOrigins: ['https://*.cloudworkstations.dev'],
